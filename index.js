e674eec40e1dc2ffd312c1832e61f70a5eee5896
Victor Mayi <victormay.i.797@gmail.com>
2022-08-16 09:20:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+function multiply(num1, num2){
+    return num1 * num2;
+};
+let num1 = 31;
+let num2 = 2;
+multiply = 31 * 2;
+console.log(multiply);
 
-// Write your code here
+function random(){
+    let Number = Math.ceil(Math.random() * 10);
+    return Number;
+};
+random = random();
+console.log(random);
+console.log(Number.isInteger(random));
+
+function mod(num3, num4){
+    return num3 % num4;
+};
+let num3 = 19;
+let num4 = 5;
+mod = 19 % 5;
+console.log(mod);
+
+function max(){
+    return Math.max(16, 4, 8, 12, 20)
+};
+max = max();
+console.log(max);
